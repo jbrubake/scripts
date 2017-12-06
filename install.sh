76de790f446bab542594366eab93a6ffbb558f57
Jeremy Brubaker <jbru362@gmail.com>
2017-12-05 20:42:32 -0500

    Rewrite install.sh to use install(1)

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ IFS='
 ##
 ## Command line options
 ##
-FORCE=              # do not overwrite files
+BACKUP=-b           # backup existing files
 VERBOSE=            # empty means be quiet
 DESTDIR="$HOME/bin" # where to install everything
 HOST=`hostname`
@@ -39,7 +39,7 @@ HOSTIGNORE="$IGNOREFILE.$HOST"    # host-specific ignore file
 while getopts "n:fd:v" opt; do
     case $opt in
         n) HOST=$OPTARG; HOSTIGNORE="$IGNOREFILE.$OPTARG" ;;
-        f) FORCE='-f' ;;
+        f) BACKUP= ;;
         d) DESTDIR=$OPTARG ;;
         v) VERBOSE='-v' ;;
         ?) exit $ERR_INVALID_OPTION ;;
@@ -54,10 +54,8 @@ fi
 
 # Action happens here, following these rules:
 #
-# - skip files in .ignore
-# - skip files in .ignore.<host> if hostname = <host>
+# - skip files in .ignore and .ignore.<host>
 # - if -f was *not* specified
-# -     Skip existing links
 # -     Backup existing files
 # -     Skip existing files if backup exists
 # - else -f *was* specified
@@ -71,22 +69,14 @@ do
         test $f = $p && continue 2 # continue OUTER loop
     done
 
-    # if file exists and -f not specified, make backups
-    if test -e "$DESTDIR/$f" || test -L "$DESTDIR/$f" &&
-        test -z $FORCE
+    # skip if -f not specified and backup exists
+    if test -e "$DESTDIR/$f~" && test $BACKUP = '-b'
     then
-        # backup existing files
-        if test -e "$DESTDIR/__$f"
-        then
-            echo "Backup __$f already exists. Skipping" >&2
-            continue
-        else
-            mv $VERBOSE $FORCE "$DESTDIR/$f" "$DESTDIR/__$f"
-        fi
+        echo "Backup $f~ already exists. Skipping" >&2
+        continue
     fi
 
     # install
-    # TODO: should really use install(1)
-    cp $VERBOSE "$f" "$DESTDIR"
+    install $VERBOSE $BACKUP -t "$DESTDIR" "$f"
 done
 
