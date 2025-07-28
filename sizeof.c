f677ab6fbab7370a0dfada930b536876abb0a6f3
Jeremy Brubaker <jbru362@gmail.com>
2025-07-28 13:58:50 -0400

    Print various stdlib limits
    Not strictly sizeof anymore but I found these to be useful when
    writing jlibc

--- sizeof.c
+++ sizeof.c
@@ -17,12 +17,14 @@
  * abstract: print sizes of C data types
  *
  */
-
+#include <limits.h>
+#include <stddef.h>
 #include <stdio.h>
 
 int
 main ()
 {
+    puts ("sizeof basic types");
     printf ("char          %lu\n", sizeof (char));
     printf ("short         %lu\n", sizeof (short));
     printf ("int           %lu\n", sizeof (int));
@@ -32,7 +34,33 @@ main ()
     printf ("double        %lu\n", sizeof (double));
     printf ("long double   %lu\n", sizeof (long double));
     printf ("size_t        %lu\n", sizeof (size_t));
-
+    printf ("ptrdiff_t     %lu\n", sizeof (ptrdiff_t));
+    printf ("wchar_t       %lu\n", sizeof (wchar_t));
+    puts ("");
+    puts ("integer limits");
+    printf ("CHAR_BIT = %i\n", CHAR_BIT);
+    printf ("UCHAR_MAX = %u\n", UCHAR_MAX);
+    printf ("CHAR_MIN = %i\n", CHAR_MIN);
+    printf ("CHAR_MAX = %i\n", CHAR_MAX);
+    printf ("SCHAR_MIN = %i\n", SCHAR_MIN);
+    printf ("SCHAR_MAX = %i\n", SCHAR_MAX);
+    printf ("MB_LEN_MAX = %i\n", MB_LEN_MAX);
+    puts ("");
+    printf ("USHRT_MAX = %u\n", USHRT_MAX);
+    printf ("SHRT_MIN = %i\n", SHRT_MIN);
+    printf ("SHRT_MAX = %i\n", SHRT_MAX);
+    puts ("");
+    printf ("UINT_MAX = %u\n", UINT_MAX);
+    printf ("INT_MIN = %i\n", INT_MIN);
+    printf ("INT_MAX = %i\n", INT_MAX);
+    puts ("");
+    printf ("ULONG_MAX = %lu\n", ULONG_MAX);
+    printf ("LONG_MIN = %li\n", LONG_MIN);
+    printf ("LONG_MAX = %li\n", LONG_MAX);
+    puts ("");
+    printf ("ULLONG_MAX = %llu\n", ULLONG_MAX);
+    printf ("LLONG_MIN = %lli\n", LLONG_MIN);
+    printf ("LLONG_MAX = %lli\n", LLONG_MAX);
     return 0;
 }
 
